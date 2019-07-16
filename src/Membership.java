d34d45b2b0c9d5887555f0c6df1bf0d29c82159d
YeoungJaeJeon <won01414@gmail.com>
2019-07-16 23:57:08 +0900

    Arraylist Add

--- src/Membership.java
+++ src/Membership.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Membership {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
+        ArrayList <Join> inputList=new ArrayList<>();
         Join input = new Join();
 
         while (true) {
         input.join();
         System.out.println();
+        inputList.add(input);
 
         System.out.println("새로운 회원가입을 하려면 yes를 입력해주세요.");
         String yes = scr.next();
@@ -18,10 +20,12 @@ public class Membership {
         else {
             break; }
         }
+        for(int index=0; index<inputList.size(); index++){
+            Join callin = inputList.get(index);
+
+
+
+
+        }
         }
     }
-
-
-
-
-
